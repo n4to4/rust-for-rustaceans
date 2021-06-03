87eb8e84c7ef3ff994b7667a2055bd5a93bd0f23
n4to4 <n4to4k@gmail.com>
2021-06-03 18:06:58 +0900

    snapshot

--- chapter02/src/main.rs
+++ chapter02/src/main.rs
@@ -1,4 +1,6 @@
-fn main() {}
+fn main() {
+    call_replace_with_84();
+}
 
 fn run1() {
     let x = 42;
@@ -26,4 +28,19 @@ fn run3() {
     }
     let x2 = x1;
     //let y2 = y1;
+}
+
+fn call_replace_with_84() {
+    let mut s = Box::new(42);
+    replace_with_84(&mut s);
+}
+
+fn replace_with_84(s: &mut Box<i32>) {
+    //let was = *s;
+    let was = std::mem::take(s);
+    *s = was;
+
+    let mut r = Box::new(84);
+    std::mem::swap(s, &mut r);
+    assert_ne!(*r, 84);
 }
